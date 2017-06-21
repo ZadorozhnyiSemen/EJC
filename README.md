7745dec13d89ffe05166f67a14261872db01c3f1
Zadorozhnyi <Zadorozhnyi@reksoft.ru>
2017-06-21 10:26:16 +0300

    Added epam.core.task_* package structure Added FirstTask with 'Hello world!' Added strategy to README.md file Deleted old Main class

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # EJC
 Epam systems training
+
+## Strategy
+
+1. Begin with `Hello world!`
+2. *Learn*
+3. Success
+4. `GOTO 2`
+
---
+++ src/main/java/epam/core/task_01/FirstTask.java
@@ -0,0 +1,7 @@
+package epam.core.task_01;
+
+public class FirstTask {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
