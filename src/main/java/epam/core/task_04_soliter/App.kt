477b654721e07d55888b7805ec2f616e6ad53b4a
Zadorozhnyi <Zadorozhnyi@reksoft.ru>
2017-07-07 17:13:46 +0300

    Added console user interation

--- src/main/java/epam/core/task_04_soliter/App.kt
+++ src/main/java/epam/core/task_04_soliter/App.kt
@@ -2,9 +2,47 @@ package epam.core.task_04_soliter
 
 fun main(args: Array<String>) {
     GameModel.resetGame()
-    GamePresenter.onDeckTap()
-    GamePresenter.onDeckTap()
-    GamePresenter.onDeckTap()
-    GameModel.debugPring()
+    var flag = true
+    println("Hello player")
+    while (flag) {
+        println("pick action (play fair there is no validation)")
+        println("You can open new card by pressing '0'")
+        println("You can tap on waste pile by pressing '9'")
+        println("You can tap on piles by pressing from '1' to '7'")
+        println("you can tap on foundation pile by pressing 'q' 'w' 'e' 'r'")
+        println("QUIT = QUIT")
+        GameModel.debugPring()
+        val userAction = readLine()
+        when (userAction) {
+            "QUIT" -> flag = false
+            "0" -> GamePresenter.onDeckTap()
+            "9" -> GamePresenter.onWasteTap()
+            "q" -> GamePresenter.onFoundationTap(0)
+            "w" -> GamePresenter.onFoundationTap(1)
+            "e" -> GamePresenter.onFoundationTap(2)
+            "r" -> GamePresenter.onFoundationTap(3)
+            "1" -> pressed(userAction.toInt())
+            "2" -> pressed(userAction.toInt())
+            "3" -> pressed(userAction.toInt())
+            "4" -> pressed(userAction.toInt())
+            "5" -> pressed(userAction.toInt())
+            "6" -> pressed(userAction.toInt())
+            "7" -> pressed(userAction.toInt())
+
+        }
+        GamePresenter.onDeckTap()
+        GamePresenter.onWasteTap()
+        GameModel.tableauPiles.forEachIndexed { index, tableauPile ->
+            GamePresenter.onTableauTap(index, tableauPile.cards.lastIndex)
+        }
+    }
+
 }
+
+fun  pressed(tableauIndex: Int) {
+    println("Enter index of card bottom-top from 0 to 12")
+    val index = readLine()
+    GamePresenter.onTableauTap(tableauIndex, index!!.toInt());
+}
+
 
