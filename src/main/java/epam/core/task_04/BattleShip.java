83a70718357f7b289798d93314e47e4ad6547ce2
Zadorozhnyi <Zadorozhnyi@reksoft.ru>
2017-07-07 13:37:57 +0300

    Add full battle ship game in one commit

---
+++ src/main/java/epam/core/task_04/BattleShip.java
@@ -0,0 +1,11 @@
+package epam.core.task_04;
+
+import epam.core.task_04.game.GameProcess;
+
+public class BattleShip {
+    public static void main(String[] args) {
+        System.err.println("Please take a look at package task_04_soliter after this one!");
+        GameProcess battleShipGame = new GameProcess();
+        battleShipGame.startGame();
+    }
+}
